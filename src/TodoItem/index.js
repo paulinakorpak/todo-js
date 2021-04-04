8b005662d3946c50cd26014916390642e7b8b948
Paulina Korpak <korpak.paulina@gmail.com>
2021-04-04 18:31:51 +0100

    Handle changing todo items status

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -9,6 +9,12 @@ export const TodoItem = (title) => {
 
   const render = (element) => {
     element.innerHTML = template(state.id, state.title, state.completed);
+    element.querySelector('input').addEventListener('change', () => toggleCompleted(element));
+  };
+
+  const toggleCompleted = (element) => {
+    state.completed = !state.completed;
+    render(element);
   };
 
   return { render };
